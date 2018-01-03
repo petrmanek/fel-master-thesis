88f8c8912429d7593eec9a801c90527c8fd01f3e
Petr Manek <petr.manek@seznam.cz>
2018-01-03 11:55:43 +0100

    cal: add edge structure

---
+++ libcal/include/cal/edge.h
@@ -0,0 +1,15 @@
+//
+// Created by petr on 3.1.18.
+//
+
+#ifndef LIBCAL_EDGE_H
+#define LIBCAL_EDGE_H
+
+#include <cstdint>
+
+struct edge {
+    std::size_t start;
+    std::size_t end;
+};
+
+#endif //LIBCAL_EDGE_H
